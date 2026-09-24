3c5450fb9abd531df4a4e77d4a237e4b04f933b7
root <root@ip-172-31-37-181.us-east-2.compute.internal>
2026-09-24 08:45:42 +0000

    lb port added

--- lb.tf
+++ lb.tf
@@ -8,7 +8,7 @@ resource "aws_elb" "mylb" {
   listener {
     instance_port     = 80
     instance_protocol = "http"
-    lb_port           = 8080
+    lb_port           = 80
     lb_protocol       = "http"
   }
 }
